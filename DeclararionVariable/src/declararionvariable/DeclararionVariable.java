fc8b339ad1349672bc9b67cc136aa3a7496da789
cymaniatico <ingluisgarciac@gmail.com>
2019-07-30 09:08:18 -0500

    first commit

---
+++ DeclararionVariable/src/declararionvariable/DeclararionVariable.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package declararionvariable;
+
+/**
+ *
+ * @author Luis Garcia
+ */
+public class DeclararionVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num;
+        double pi = 3.14;
+        char caracter = 'a';
+        boolean decision = true;
+        num = 3;
+        // Este es un comentario de una sola linea
+        /* Este es un comentario en bloque
+        puedo hacer las lineas que quiera*/
+        
+        System.out.print(num+"\t");
+        System.out.println(pi);
+        System.out.println("El caracter almacanenado es "+caracter);
+    }
+    
+}
